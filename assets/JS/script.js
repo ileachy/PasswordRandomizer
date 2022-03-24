7170485cefb08a70d65c4dfc6a66807afb97b6d5
Ian L <ileach81@gmail.com>
2022-03-24 03:34:03 -0400

    Added character choices and started pass function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+let num = "0123456789";
+let lowerC = "abcdefghijklmnopqrstuvwxyz";
+let upperC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let specialC = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
+// Create password
+function generatePass() {
+  let choice = "";
+  let length = prompt("Select a password length between 8 and 128 characters");
+  while (length <= 7 || length >= 129) {
+    alert("Password must be between 8 and 128 characters");
+    let length = prompt(
+      "Please select a password length between 8 and 128 characters"
+    );
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +25,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
