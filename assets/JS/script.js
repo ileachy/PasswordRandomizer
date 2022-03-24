977b339832598fb9a76864d2aebd8c26adc906bf
Ian L <ileach81@gmail.com>
2022-03-24 03:41:54 -0400

    choice prompts

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,13 +6,25 @@ let specialC = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
 // Create password
 function generatePass() {
-  let choice = "";
+  let pass = "";
   let length = prompt("Select a password length between 8 and 128 characters");
   while (length <= 7 || length >= 129) {
     alert("Password must be between 8 and 128 characters");
     let length = prompt(
       "Please select a password length between 8 and 128 characters"
     );
+  }
+
+  let numChoice = confirm("Should password have numbers?");
+  let lowerChoice = confirm("Should password have lowercase letters?");
+  let upperChoice = confirm("Should password have uppercase letters?");
+  let specChoice = confirm("Should password have special characters?");
+  while (!numChoice && !lowerChoice && !upperChoice && !specChoice) {
+    alert("At least 1 must be selected!");
+    let numChoice = confirm("Should password have numbers?");
+    let lowerChoice = confirm("Should password have lowercase letters?");
+    let upperChoice = confirm("Should password have uppercase letters?");
+    let specChoice = confirm("Should password have special characters?");
   }
 }
 
