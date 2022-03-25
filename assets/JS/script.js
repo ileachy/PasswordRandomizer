5776f6f8153f9644010832a8f96d799e429db701
Ian L <ileach81@gmail.com>
2022-03-24 20:18:09 -0400

    added more comments

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -46,11 +46,12 @@ function generatePassword() {
       "Please select a password length between 8 and 128 characters"
     );
   }
-
+  // char selections
   let numChoice = confirm("Should password have numbers?");
   let lowerChoice = confirm("Should password have lowercase letters?");
   let upperChoice = confirm("Should password have uppercase letters?");
   let specChoice = confirm("Should password have special characters?");
+  // if no selection, re-send
   while (
     numChoice !== true &&
     lowerChoice !== true &&
@@ -63,6 +64,7 @@ function generatePassword() {
     let upperChoice = confirm("Should password have uppercase letters?");
     let specChoice = confirm("Should password have special characters?");
   }
+  // puts pass selections together
   let charControl = lowerC.concat(upperC, num, specialC);
   // randomization
   let ranPass = "";
